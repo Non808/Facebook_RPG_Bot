72fe628787952fc5fcff7306784498ff81bd08be
Noah <34938698+Non808@users.noreply.github.com>
2018-09-16 19:53:32 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,11 +1,21 @@
 import fbchat
 
 class CustomClient(fbchat.Client):
-  def SEND(self,txt,thid,thg):
-    self.send(fbchat.Message(text=txt),thread_id=thid,thread_type=thg)
-  
-  def onMessage(self,mid,author_id,message_object,thread_id,thread_type,ts,metadata,msg,**kwargs):
-    pass
+	def getAuthor(self,author_id):
+		a = str(self.fetchUserInfo(author_id)[autor_id])
+		nam = ""
+		for c in a[5:]:
+			if c == "(":
+				break
+			else:
+				nam += c
+		return nam[1:-1]
+	
+	def SEND(self,txt,thid,thg):
+		self.send(fbchat.Message(text=txt),thread_id=thid,thread_type=thg)
+    
+	def onMessage(self,mid,author_id,message_object,thread_id,thread_type,ts,metadata,msg,**kwargs):
+		pass
   
 
 
