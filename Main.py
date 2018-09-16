1b3961f221891ab0d24bae944e1926633a8f7d14
Noah <34938698+Non808@users.noreply.github.com>
2018-09-16 18:30:47 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,14 @@
+import fbchat
 
+class CustomClient(fbchat.Client):
+  def SEND(self,txt,thid,thg):
+    self.send(fbchat.Message(text=txt),thread_id=thid,thread_type=thg)
+  
+  def onMessage(self,mid,author_id,message_object,thread_id,thread_type,ts,metadata,msg,**kwargs):
+    pass
+  
+
+
+client = CustomClient("botymcbotface808@gmail.com","[Password]")
+
+client.listen()
